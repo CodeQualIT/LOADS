de034ff101517e37d0c90a4c0b3773fb0e65e3b9
CC007 <Coolcat_the_best2@Hotmail.com>
2024-06-25 10:37:47 +0200

    Fix visibility of methods and use wildcard for container types

--- implementations/kotlin/src/main/kotlin/nl/cqit/loads/Decoder.kt
+++ implementations/kotlin/src/main/kotlin/nl/cqit/loads/Decoder.kt
@@ -4,15 +4,22 @@ package nl.cqit.loads
 
 import nl.cqit.loads.utils.toUByteArray
 import kotlin.reflect.*
+import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.primaryConstructor
 import kotlin.text.Charsets.UTF_8
 
-private val ARRAY_START: UByte = 0xFAu
-private val BINARY_VALUE: UByte = 0xFBu
-private val OBJECT_START: UByte = 0xFCu
-private val NULL_VALUE: UByte = 0xFDu
-private val CONTAINER_END: UByte = 0xFEu
-private val ELEMENT_SEPARATOR: UByte = 0xFFu
+private const val INVALID_STRING_CHARACTER_MSG = "Invalid character(s) found in string value: "
+private const val INVALID_ARRAY_START_MSG = "Invalid start of array container at position "
+private const val INVALID_OBJECT_START_MSG = "Invalid start of object container at position "
+private const val EXPECTED_ELEMENT_SEPARATOR_MSG =
+    "Unexpected character found. Expected to find an element separator at position "
+
+private const val ARRAY_START: UByte = 0xFAu
+private const val BINARY_VALUE: UByte = 0xFBu
+private const val OBJECT_START: UByte = 0xFCu
+private const val NULL_VALUE: UByte = 0xFDu
+private const val CONTAINER_END: UByte = 0xFEu
+private const val ELEMENT_SEPARATOR: UByte = 0xFFu
 
 private val SPECIAL_BYTES = ubyteArrayOf(
     ARRAY_START,
@@ -22,7 +29,10 @@ private val SPECIAL_BYTES = ubyteArrayOf(
     CONTAINER_END,
     ELEMENT_SEPARATOR
 )
-private val VALUE_TERMINATORS = ubyteArrayOf(ELEMENT_SEPARATOR, CONTAINER_END)
+private val VALUE_TERMINATORS = ubyteArrayOf(
+    ELEMENT_SEPARATOR,
+    CONTAINER_END
+)
 
 private fun elemType(type: KType) = type.arguments[0].type!!
 private fun keyType(type: KType) = type.arguments[0].type!!
@@ -53,18 +63,18 @@ fun main() {
         0xFEu,
         0xFEu
     )
-    val result: Obj? = decode(data)
+    val result: Obj = decode(data)
     println(result)
 }
 
-inline fun <reified T : Any> decode(data: UByteArray): T? {
+inline fun <reified T : Any> decode(data: UByteArray): T {
     val type = typeOf<T>()
-    return decode(type, data, 0).second as T?
+    return decode(type, data, 0).second as T
 }
 
-fun decode(type: KType, data: UByteArray, offset: Int): Pair<Int, Any?> {
-    val (newOffset: Int, result: Any) = when {
-        type.classifier == Array::class -> toArray(type, data, offset)
+fun decode(type: KType, data: UByteArray, offset: Int): Pair<Int, *> {
+    val (newOffset: Int, result: Any?) = when {
+        type.isSubtypeOf(typeOf<Array<*>>()) -> toArray(type, data, offset)
         type.classifier == List::class -> toList(type, data, offset)
         type.classifier == Set::class -> toSet(type, data, offset)
         type.classifier == Collection::class -> toCollection(type, data, offset)
@@ -93,47 +103,50 @@ fun decode(type: KType, data: UByteArray, offset: Int): Pair<Int, Any?> {
     return newOffset to result
 }
 
-fun isData(classifier: KClassifier?): Boolean =
+private fun isData(classifier: KClassifier?): Boolean =
     classifier is KClass<*> && classifier.isData
 
-fun toString(data: UByteArray, offset: Int): Pair<Int, String> {
+
+private fun toString(data: UByteArray, offset: Int): Pair<Int, String> {
     var i = offset
     while (i < data.size && data[i] !in VALUE_TERMINATORS) i++
     val slice = data.sliceArray(offset until i)
     val specialCharactersInString = slice.intersect(SPECIAL_BYTES)
-    if (specialCharactersInString.isNotEmpty()) {
+    require (specialCharactersInString.isEmpty()) {
         val invalidCharacters = specialCharactersInString.map {
             val hex = it.toHexString(HexFormat.UpperCase)
             val pos = slice.indexOf(it) + offset
             "Ox$hex at position $pos"
         }
-        throw IllegalArgumentException(
-            "Invalid character(s) found in string value: ${invalidCharacters.joinToString()}"
-        )
+        INVALID_STRING_CHARACTER_MSG + invalidCharacters.joinToString()
     }
     return i to String(slice.toByteArray())
 }
 
-fun toArray(elementType: KType, data: UByteArray, offset: Int): Pair<Int, Array<Any?>> =
-    toArrayContainer(elementType, data, offset, MutableList<Any?>::toTypedArray)
+private fun toArray(elementType: KType, data: UByteArray, offset: Int): Pair<Int, Array<*>> =
+    toArrayContainer(elementType, data, offset, MutableList<*>::toTypedArray)
 
-fun toList(elementType: KType, data: UByteArray, offset: Int): Pair<Int, List<Any?>> =
-    toArrayContainer(elementType, data, offset, MutableList<Any?>::toList)
+private fun toList(elementType: KType, data: UByteArray, offset: Int): Pair<Int, List<*>> =
+    toArrayContainer(elementType, data, offset, MutableList<*>::toList)
 
-fun toSet(elementType: KType, data: UByteArray, offset: Int): Pair<Int, Set<Any?>> =
-    toArrayContainer(elementType, data, offset, MutableList<Any?>::toSet)
+private fun toSet(elementType: KType, data: UByteArray, offset: Int): Pair<Int, Set<*>> =
+    toArrayContainer(elementType, data, offset, MutableList<*>::toSet)
 
-fun toCollection(elementType: KType, data: UByteArray, offset: Int): Pair<Int, Collection<Any?>> =
-    toArrayContainer(elementType, data, offset, MutableList<Any?>::toList)
+private fun toCollection(
+    elementType: KType,
+    data: UByteArray,
+    offset: Int
+): Pair<Int, Collection<*>> =
+    toArrayContainer(elementType, data, offset, MutableList<*>::toList)
 
-fun toMap(type: KType, data: UByteArray, offset: Int): Pair<Int, Map<Any?, Any?>> {
+private fun toMap(type: KType, data: UByteArray, offset: Int): Pair<Int, Map<*, *>> {
     return toObjectContainer(
         keyType(type), { it }, { valueType(type) },
-        data, offset, MutableMap<Any?, Any?>::toMap
+        data, offset, MutableMap<*, *>::toMap
     )
 }
 
-fun toData(type: KType, data: UByteArray, offset: Int): Pair<Int, Any> {
+private fun toData(type: KType, data: UByteArray, offset: Int): Pair<Int, *> {
     val dataClass = type.classifier as KClass<*>
     val constructor = dataClass.primaryConstructor!!
     return toObjectContainer(
@@ -146,13 +159,14 @@ private operator fun List<KParameter>.get(key: Any?) =
     this.firstOrNull { it.name == key }
         ?: throw IllegalArgumentException("Unknown key found: $key")
 
-fun <T> toArrayContainer(
+
+private fun <T> toArrayContainer(
     type: KType,
     data: UByteArray,
     offset: Int,
-    containerMapper: (MutableList<Any?>) -> T
+    containerMapper: (MutableList<*>) -> T
 ): Pair<Int, T> {
-    if (data[offset] != ARRAY_START) throw IllegalArgumentException("Invalid start of element container")
+    require (data[offset] == ARRAY_START) { INVALID_ARRAY_START_MSG + offset }
     val list = mutableListOf<Any?>()
     var i = offset + 1
     while (true) {
@@ -162,21 +176,22 @@ fun <T> toArrayContainer(
 
         list.add(element)
         if (data[i] == CONTAINER_END) break
-        if (data[i] != ELEMENT_SEPARATOR) throw IllegalArgumentException("Unexpected character found. Expected to find an element separator")
+        require(data[i] == ELEMENT_SEPARATOR) { EXPECTED_ELEMENT_SEPARATOR_MSG + i }
         i++
     }
-    return i + 1 to containerMapper.invoke(list)
+    return i + 1 to containerMapper(list)
 }
 
-fun <T, U> toObjectContainer(
+
+private fun <T, U> toObjectContainer(
     keyType: KType,
     keyMapper: (Any?) -> U,
     valueTypeResolver: (U) -> KType,
     data: UByteArray,
     offset: Int,
-    containerMapper: (MutableMap<U, Any?>) -> T
+    containerMapper: (MutableMap<U, *>) -> T
 ): Pair<Int, T> {
-    if (data[offset] != OBJECT_START) throw IllegalArgumentException("Invalid start of object container")
+    require(data[offset] == OBJECT_START) { INVALID_OBJECT_START_MSG + offset }
     val map = mutableMapOf<U, Any?>()
     var i = offset + 1
     while (true) {
@@ -184,7 +199,7 @@ fun <T, U> toObjectContainer(
         i = decodeKey.first
         val key = keyMapper(decodeKey.second)
 
-        if (data[i] != ELEMENT_SEPARATOR) throw IllegalArgumentException("Unexpected character found. Expected to find an element separator")
+        require(data[i] == ELEMENT_SEPARATOR) { EXPECTED_ELEMENT_SEPARATOR_MSG + i }
         i++
 
         val valueType = valueTypeResolver(key)
@@ -194,7 +209,7 @@ fun <T, U> toObjectContainer(
 
         map[key] = value
         if (data[i] == CONTAINER_END) break
-        if (data[i] != ELEMENT_SEPARATOR) throw IllegalArgumentException("Unexpected character found. Expected to find an element separator")
+        require(data[i] == ELEMENT_SEPARATOR) { EXPECTED_ELEMENT_SEPARATOR_MSG + i }
         i++
     }
     return i + 1 to containerMapper.invoke(map)
---
+++ implementations/kotlin/src/test/kotlin/nl/cqit/loads/DecoderKtTest.kt
@@ -0,0 +1,43 @@
+@file:OptIn(ExperimentalUnsignedTypes::class)
+
+package nl.cqit.loads
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DecoderKtTest {
+
+    @Test
+    fun `decode string`() {
+        // prepare
+        val input = ubyteArrayOf(0x31u, 0x32u, 0x33u)
+
+        // execute
+        val actual: String = decode(input)
+
+        // verify
+        val expected = "123"
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `decode array of strings`() {
+        // prepare
+        val input = ubyteArrayOf(
+            0xFAu,
+            0x31u, 0x32u, 0x33u,
+            0xFFu,
+            0x34u, 0x35u, 0x36u,
+            0xFFu,
+            0x37u, 0x38u, 0x39u,
+            0xFEu,
+        )
+
+        // execute
+        val actual: Array<String> = decode(input)
+
+        // verify
+        val expected = arrayOf("123", "456", "789")
+        assertThat(actual).isEqualTo(expected)
+    }
+}
