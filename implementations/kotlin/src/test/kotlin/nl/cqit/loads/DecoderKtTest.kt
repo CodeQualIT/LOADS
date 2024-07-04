f4d23c308e780bea3c30e4e13b308ec5e99958eb
CC007 <Coolcat_the_best2@Hotmail.com>
2024-07-04 22:42:29 +0200

    Test decoding lists, sets and collections

--- implementations/kotlin/src/test/kotlin/nl/cqit/loads/DecoderKtTest.kt
+++ implementations/kotlin/src/test/kotlin/nl/cqit/loads/DecoderKtTest.kt
@@ -21,6 +21,71 @@ class DecoderKtTest {
     }
 
     @Test
+    fun `decode list of strings`() {
+        // prepare
+        val input = ubyteArrayOf(
+            0xFAu,
+            0x31u, 0x32u, 0x33u,
+            0xFFu,
+            0x34u, 0x35u, 0x36u,
+            0xFFu,
+            0x37u, 0x38u, 0x39u,
+            0xFEu,
+        )
+
+        // execute
+        val actual: List<String> = decode(input)
+
+        // verify
+        val expected = listOf("123", "456", "789")
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `decode set of strings`() {
+        // prepare
+        val input = ubyteArrayOf(
+            0xFAu,
+            0x31u, 0x32u, 0x33u,
+            0xFFu,
+            0x34u, 0x35u, 0x36u,
+            0xFFu,
+            0x37u, 0x38u, 0x39u,
+            0xFEu,
+        )
+
+        // execute
+        val actual: Set<String> = decode(input)
+
+        // verify
+        val expected = setOf("123", "456", "789")
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `decode collection of strings`() {
+        // prepare
+        val input = ubyteArrayOf(
+            0xFAu,
+            0x31u, 0x32u, 0x33u,
+            0xFFu,
+            0x34u, 0x35u, 0x36u,
+            0xFFu,
+            0x37u, 0x38u, 0x39u,
+            0xFEu,
+        )
+
+        // execute
+        val actual: Collection<String> = decode(input)
+
+        // verify
+        val expected = setOf("123", "456", "789")
+        assertThat(actual)
+            .containsAll(expected)
+            .isInstanceOf(Collection::class.java)
+    }
+
+    @Test
     fun `decode array of strings`() {
         // prepare
         val input = ubyteArrayOf(
